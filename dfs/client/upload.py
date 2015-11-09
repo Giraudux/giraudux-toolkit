99066d3bb0adb0d061326cba7db65035948e0731
Giraudux <giraudux@gmail.com>
2015-11-10 00:16:39 +0100

    fix protocol/meta

--- dfs/client/upload.py
+++ dfs/client/upload.py
@@ -11,47 +11,48 @@ def upload_http_post_b64_sha512(block, meta):
   request.add_header("Content-Type", "application/x-www-form-urlencoded;charset=ascii")
   with urllib.request.urlopen(request, data) as reply:
     if block_b64_sha512 == reply.read().decode():
-      return {"protocol": "http_get_b64_sha512", "hash": block_b64_sha512, "get": meta["get"]+"?id="+block_b64_sha512}
+      return ("http_get_b64_sha512", {"hash": block_b64_sha512, "get": meta["get"]+"?id="+block_b64_sha512})
   raise Exception("Failure")
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument("-b", "--block", default=785664, type=int, required=False)
-  parser.add_argument("-i", "--input", type=argparse.FileType("rb"), required=True)
-  parser.add_argument("-o", "--output", type=argparse.FileType("w"), required=True)
-  parser.add_argument("-p", "--providers", type=argparse.FileType("r"), required=True)
-  parser.add_argument("-t", "--threads", default=8, type=int, required=False)
+  parser.add_argument("-f", "--file",      type=argparse.FileType("rb"), required=True)
+  parser.add_argument("-m", "--meta",      type=argparse.FileType("w"),  required=True)
+  parser.add_argument("-p", "--providers", type=argparse.FileType("r"),  required=True)
+  parser.add_argument("-b", "--block",     type=int,                     required=False, default=785664)
+  parser.add_argument("-t", "--threads",   type=int,                     required=False, default=8)
   args = parser.parse_args()
   providers = json.load(args.providers)
   args.providers.close()
-  output = dict()
   size = 0
+  meta = dict()
   pieces = list()
-  hash = hashlib.sha512()
-  block = args.input.read(args.block)
+  file_sha512 = hashlib.sha512()
+  block = args.file.read(args.block)
   while block:
-    hash.update(block)
+    file_sha512.update(block)
     size += len(block)
     piece = dict()
     piece_providers = list()
     for provider in providers:
       try:
         # use getattr(module, provider["protocol"])
-        piece_providers.append(upload_http_post_b64_sha512(block, provider["meta"]))
+        (protocol, protocol_meta) = upload_http_post_b64_sha512(block, provider["meta"])
+        piece_providers.append({"protocol": protocol, "meta": protocol_meta})
       except:
         print()
     piece["size"] = len(block)
     piece["hash"] = hashlib.sha512(block).hexdigest()
     piece["providers"] = piece_providers
     pieces.append(piece)
-    block = args.input.read(args.block)
-  output["name"] = args.input.name
-  output["size"] = size
-  output["hash"] = hash.hexdigest()
-  output["pieces"] = pieces
-  json.dump(output, args.output)
-  args.input.close()
-  args.output.close()
+    block = args.file.read(args.block)
+  meta["name"] = args.file.name
+  meta["size"] = size
+  meta["hash"] = file_sha512.hexdigest()
+  meta["pieces"] = pieces
+  json.dump(meta, args.meta)
+  args.file.close()
+  args.meta.close()
 
 if __name__ == "__main__":
   main()
