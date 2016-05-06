a2d4c493749d1a7655dd7ace2394bbad73ca77fc
Giraudux <giraudux@gmail.com>
2016-05-06 03:24:11 +0200

    new arch

--- ftp/ftp_checker.py
+++ ftp/ftp_checker.py
@@ -2,65 +2,98 @@
 
 # Alexis Giraudet
 
-from ftplib import FTP
+from ftplib import FTP, FTP_TLS
 from sys import argv
 from threading import Thread, Lock
 from urllib.request import urlopen
 from xml.etree import ElementTree
+from itertools import chain
+from base64 import b64decode
 
-class ftp_checker:
-	def __init__(self, timeout=3):
-		self._io_mtx = Lock()
-		self._timeout = int(timeout)
-	def check_ftp_from_url(self, url):
-	    with urlopen(url) as f:
-	    	self.check_ftp_from_data(f)
-	def check_ftp_from_data(self, data):
-		try:
-			thread_group = list()
-			root = ElementTree.parse(data).getroot()
-			for server in root.iter('Server'):
-				t = Thread(target=self.check_ftp, args=(server.find('Host').text, server.find('Port').text, server.find('User').text, server.find('Pass').text, server.find('Protocol').text))
-				thread_group.append(t)
-				t.start()
-			for server in root.iter('LastServer'):
-				t = Thread(target=self.check_ftp, args=(server.find('Host').text, server.find('Port').text, server.find('User').text, server.find('Pass').text, server.find('Protocol').text))
-				thread_group.append(t)
-				t.start()
-			for t in thread_group:
-				t.join()
-		except Exception as e:
-			print('Exception: {}'.format(e))
-	def check_ftp(self, host='', port='', user='', passwd='', protocol=''):
-		pro = int()
-		res = str()
-		ftp = None
-		try:
-			if(pro == 0):
-				ftp = FTP()
-			elif(pro == 1):
-				ftp = FTP_TLS()
-			else:
-				raise Exception('unknown protocol')
-			ftp.connect(host, int(port), self._timeout)
-			ftp.login(user, passwd)
-			ftp.quit()
-			res = 'OK'
-		except Exception as e:
-			ftp.close()
-			res = 'Exception: {}'.format(e)
-		with self._io_mtx:
-			print('Check: {}@{}:{} {}'.format(user, host, port, passwd))
-			print(res)
-			print()
+#todo: use opt arg to read files/URLs parameters
+#todo: manage base64 passwords
+#todo: output results to sqlite, json or standard output
+#todo: set max thread
+
+class FileZillaXmlChecker:
+
+    def __init__(self, database=None, timeout = 8):
+        self._io_mtx = Lock()
+        self._database = database
+        self._timeout = timeout
+
+    def checkUrls(self, urls):
+        try:
+            threads = list()
+
+            for url in urls:
+                thread = Thread(target = self.checkFromUrl, args = (url,))
+                threads.append(thread)
+                thread.start()
+
+            for thread in threads:
+                thread.join()
+        except Exception as e:
+            print(e)
+
+    def checkFromUrl(self, url):
+        try:
+            with urlopen(url, timeout = self._timeout) as f:
+                threads = list()
+                root = ElementTree.parse(f).getroot()
+
+                for server in chain(root.iter('Server'), root.iter('LastServer')):
+                    thread = Thread(target = self.checkFromServer, args = (server,))
+                    threads.append(thread)
+                    thread.start()
+
+                for thread in threads:
+                    thread.join()
+        except Exception as e:
+            print(e)
+
+    def checkFromServer(self, server):
+        try:
+            serverHost = server.find('Host').text
+            serverPort = server.find('Port').text
+            serverUser = server.find('User').text
+            serverPass = server.find('User')
+            serverProtocol = server.find('Protocol').text
+
+            if serverPass.get("encoding") == "base64":
+                serverPass = b64decode(serverPass.text.encode()).decode()
+            else:
+                serverPass = serverPass.text
+
+            self.checkFtp(serverHost, serverPort, serverUser, serverPass, serverProtocol)
+
+            print("SUCCESS {} {} {} {} {}".format(serverHost, serverPort, serverUser, serverPass, serverProtocol))
+        except Exception as e:
+            print(e)
+
+    def checkFtp(self, host, port, user, passwd, protocol):
+        port = int(port)
+        protocol = int(protocol)
+        try:
+            if(protocol == 0):
+                with FTP(timeout = self._timeout) as ftp:
+                    ftp.connect(host, port)
+                    ftp.login(user, passwd)
+            elif(protocol == 1):
+                with FTP_TLS(timeout = self._timeout) as ftp_tls:
+                    ftp_tls.connect(host, port)
+                    ftp_tls.login(user, passwd)
+            else:
+                raise Exception("unknown protocol: {}".format(protocol))
+        except Exception as e:
+            print(e)
 
 def main():
-	if (len(argv) > 1):
-		checker = ftp_checker()
-		for i in range(1, len(argv)):
-			checker.check_ftp_from_url(argv[i])
-	else:
-		print('Usage: ftp_checker.py [URL]...')
+    if (len(argv) > 1):
+        checker = FileZillaXmlChecker()
+        checker.checkUrls(argv[1:])
+    else:
+        print("Usage : {} [URL]...".format(argv[0]))
 
 if __name__ == "__main__":
     main()
